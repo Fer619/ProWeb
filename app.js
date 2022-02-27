73bc10f2387be662b3bca84958affe4af52fe09a
Fer619 <fbriceno@ucol.mx>
2022-02-27 17:00:59 -0600

    object persona

---
+++ app.js
@@ -0,0 +1,12 @@
+// let obj = {};
+
+let persona = {
+    nombre: "Fernando",
+    apellido: "Briceño"
+}
+
+console.log(persona); // llama todos los valores del objeto persona
+console.log(persona.nombre); // llama solo el valor nombre del objeto persona
+console.log(persona.apellido); // lama solo el valor apellido del objeto persona
+
+
