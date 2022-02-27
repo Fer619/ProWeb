90d7c85ed3bb2b960ff6a21984e38a3c4454439c
Fer619 <fbriceno@ucol.mx>
2022-02-27 17:17:56 -0600

    object persona.domicilio

--- app.js
+++ app.js
@@ -2,11 +2,25 @@
 
 let persona = {
     nombre: "Fernando",
-    apellido: "Briceño"
+    apellido: "Briceño",
+    getName: function() {
+        return `${this.nombre} ${this.apellido}`
+    },
+
+    domicilio: {
+        calle: "Galicia #703",
+        colonia: "Villas Diamante",
+        cp: 28986,
+        municipio: "Villa de Alvarez"
+    }
 }
 
-console.log(persona); // llama todos los valores del objeto persona
-console.log(persona.nombre); // llama solo el valor nombre del objeto persona
-console.log(persona.apellido); // lama solo el valor apellido del objeto persona
+// Nota después de varios node app no cambiaban los valores fue necesario cambiar de terminal para corregir el error 
+
+//console.log(persona); // llama todos los valores del objeto persona
+//console.log(persona.nombre); // llama solo el valor nombre del objeto persona
+//console.log(persona.apellido); // lama solo el valor apellido del objeto persona.
+// console.log(persona.getName()); // llama la función getName y regresa el nombre y apellidos juntos
+console.log(persona.domicilio); // regresa solo los valores del objeto domicilio (calle, colonia, cp mun).
 
 
