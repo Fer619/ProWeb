1c0bafd00bc15270b2c8a242d0a5bff74bde10d7
Fer619 <fbriceno@ucol.mx>
2022-02-27 17:41:33 -0600

    function getDomicilio

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ let persona = {
         calle: "Galicia #703",
         colonia: "Villas Diamante",
         cp: 28986,
-        municipio: "Villa de Alvarez"
+        municipio: "Villa de Alvarez",
+        getDomicilio: function() {
+            return `${this.calle} ${"Colonia:" + this.colonia} ${"CP:" + this.cp + ","} ${this.municipio}`
+        }
     }
 }
 
@@ -20,7 +23,8 @@ let persona = {
 //console.log(persona); // llama todos los valores del objeto persona
 //console.log(persona.nombre); // llama solo el valor nombre del objeto persona
 //console.log(persona.apellido); // lama solo el valor apellido del objeto persona.
-// console.log(persona.getName()); // llama la función getName y regresa el nombre y apellidos juntos
-console.log(persona.domicilio); // regresa solo los valores del objeto domicilio (calle, colonia, cp mun).
+console.log(persona.getName()); // llama la función getName y regresa el nombre y apellidos juntos
+//console.log(persona.domicilio); // regresa solo los valores del objeto domicilio (calle, colonia, cp mun).
+console.log(persona.domicilio.getDomicilio());
 
 
